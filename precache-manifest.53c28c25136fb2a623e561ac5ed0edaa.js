1cbba6372f01c37d96f529c7c7fc6904cfe581a5
krystiano13 <krystiano13@interklasa.pl>
2020-10-18 15:46:36 +0200

    Updates

---
+++ precache-manifest.53c28c25136fb2a623e561ac5ed0edaa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5e110bea95e50bc89007f61be6deb22",
+    "url": "/zdrowie/index.html"
+  },
+  {
+    "revision": "e6b789f95cf4b483b17d",
+    "url": "/zdrowie/static/css/main.06b05995.chunk.css"
+  },
+  {
+    "revision": "5a318b6d0dd9a8e74968",
+    "url": "/zdrowie/static/js/2.d637785d.chunk.js"
+  },
+  {
+    "revision": "85c1ecd325742a4c0551d2653970c6fb",
+    "url": "/zdrowie/static/js/2.d637785d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6b789f95cf4b483b17d",
+    "url": "/zdrowie/static/js/main.fa5818d8.chunk.js"
+  },
+  {
+    "revision": "2b849a11649445c6073b",
+    "url": "/zdrowie/static/js/runtime-main.6629fd1d.js"
+  }
+]);
