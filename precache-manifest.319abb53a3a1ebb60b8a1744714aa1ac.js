56a4ea0d9e7df247de9b023b00b5962b27549d47
krystiano13 <krystiano13@interklasa.pl>
2020-10-22 20:32:08 +0200

    Updates

--- precache-manifest.319abb53a3a1ebb60b8a1744714aa1ac.js
+++ precache-manifest.319abb53a3a1ebb60b8a1744714aa1ac.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "cbc8d8c502da0f9765e4da8fcc57b0d7",
+    "revision": "f60a8b00fe1a11c0684af90de0a7e546",
     "url": "/zdrowie/index.html"
   },
   {
-    "revision": "b97a77c1247229f10906",
-    "url": "/zdrowie/static/css/main.9ee4d7fa.chunk.css"
+    "revision": "e5dfdb2311bb8d4f33f7",
+    "url": "/zdrowie/static/css/main.2418e5c7.chunk.css"
   },
   {
     "revision": "c371511916f815c0dd51",
@@ -16,7 +16,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/zdrowie/static/js/2.f92392db.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "b97a77c1247229f10906",
+    "revision": "e5dfdb2311bb8d4f33f7",
     "url": "/zdrowie/static/js/main.1ab3e8a3.chunk.js"
   },
   {
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/zdrowie/precache-manifest.cb8133b35e5b01483add97ceb90ab2eb.js"
+  "/zdrowie/precache-manifest.319abb53a3a1ebb60b8a1744714aa1ac.js"
 );
 
 self.addEventListener('message', (event) => {
